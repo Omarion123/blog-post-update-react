2253c173e97344b62c9163f45ae3aacf9362dc89
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 <anzymen@gmail.com>
2023-10-21 23:09:50 +0200

    added some login register and forgot password model

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,38 +1,184 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { BiSolidHide } from "react-icons/bi";
-const Login = () => {
+const Login = ({ setOpenModal }) => {
+  const [registerActive, setRegisterActive] = useState(false);
+  const [loginActive, setLoginActive] = useState(true);
+  const [forgotActive, setForgotActive] = useState(false);
   return (
-    <div className="login-container">
-      <div className="login-form">
-        <div className="login-header">
-          <h1>Login Form</h1>
-          <p>
-            New to <span>OMY-BLOGS </span>
-            <span className="click-to-register">
-              <Link to="/register">Register</Link>
-            </span>
-          </p>
-        </div>
-        <div className="login-username">
-          <input
-            type="text"
-            placeholder="Enter username..."
-            className="username-focus"
-          />
-        </div>
-        <div className="login-password">
-          <input type="password" placeholder="Enter password..." />
-          <BiSolidHide className="login-password-hide-icon" />
-        </div>
-        <div className="forgot-password">
-          <Link to="/forgot-password">Forgot Password?</Link>
-        </div>
-        <div className="login-button">
-          <Link to="/dashboard">
-            <h1>Login</h1>
-          </Link>
-        </div>
+    <div className="modalBackground">
+      <div className="modalContainer">
+        {loginActive && (
+          <div className="login-container">
+            <div className="login-form">
+              <div className="login-header">
+                <h1>Login Form</h1>
+                {/* <span>New to OMY-BLOGS </span> */}
+                <p>
+                  <span className="click-to-register">
+                    {/* <Link
+                      nClick={() => {
+                        setRegisterActive(true);
+                        setLoginActive(false);
+                      }}
+                    >
+                      Register
+                    </Link> */}
+                    <button
+                      className="login-btn reg-btn"
+                      onClick={() => {
+                        setRegisterActive(true);
+                        setLoginActive(false);
+                      }}
+                    >
+                      Register
+                    </button>
+                  </span>
+                </p>
+                <button
+                  className="cancelBtn"
+                  onClick={() => {
+                    setOpenModal(false);
+                  }}
+                >
+                  X
+                </button>
+              </div>
+              <div className="login-username">
+                <input
+                  type="text"
+                  placeholder="Enter username..."
+                  className="username-focus"
+                />
+              </div>
+              <div className="login-password">
+                <input type="password" placeholder="Enter password..." />
+                <BiSolidHide className="login-password-hide-icon" />
+              </div>
+              <div className="forgot-password">
+                {/* <Link to="/forgot-password">Forgot Password?</Link> */}
+                <button
+                  className="forgot-btn"
+                  onClick={() => {
+                    setRegisterActive(false);
+                    setLoginActive(false);
+                    setForgotActive(true);
+                  }}
+                >
+                  Forgot Password?
+                </button>
+              </div>
+              <div
+                className="login-button"
+                onClick={() => {
+                  setOpenModal(false);
+                }}
+              >
+                <Link to="/dashboard">
+                  <h1>Login</h1>
+                </Link>
+              </div>
+            </div>
+          </div>
+        )}
+        {registerActive && (
+          <div className="register-container">
+            <div className="login-container">
+              <div className="login-form">
+                <div className="login-header">
+                  <h1 className="reg-form-header">Register Form</h1>
+                  <p>Already have an account?</p>
+                  <span className="click-to-register">
+                    {/* <Link to="/login">Login</Link> */}
+                    <button
+                      className="login-btn"
+                      onClick={() => {
+                        setRegisterActive(false);
+                        setForgotActive(false);
+                        setLoginActive(true);
+                      }}
+                    >
+                      Login
+                    </button>
+                  </span>
+                </div>
+                <div className="login-username">
+                  <input
+                    type="text"
+                    placeholder="Enter first name..."
+                    className="username-focus"
+                  />
+                </div>
+                <div className="login-username">
+                  <input
+                    type="text"
+                    placeholder="Enter last name..."
+                    className="username-focus"
+                  />
+                </div>
+                <div className="login-username">
+                  <input
+                    type="text"
+                    placeholder="Enter username..."
+                    className="username-focus"
+                  />
+                </div>
+                <div className="login-password">
+                  <input type="password" placeholder="Enter password..." />
+                  <BiSolidHide className="login-password-hide-icon" />
+                </div>
+                <div className="login-button register-button">
+                  <h1>Register Now</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+        {forgotActive && (
+          <div className="register-container">
+            <div className="login-container">
+              <div className="login-form">
+                <div className="login-header">
+                  <h1 className="reg-form-header">Forgot password</h1>
+                  {/* <p>
+                  Already have an account? */}
+                  {/* <span className="click-to-register">
+                    <Link to="/login">Login</Link>
+                  </span> */}
+                  <button
+                    className="login-btn"
+                    onClick={() => {
+                      setRegisterActive(false);
+                      setForgotActive(false);
+                      setLoginActive(true);
+                    }}
+                  >
+                    Login
+                  </button>
+                  {/* </p> */}
+                </div>
+                <div className="login-username">
+                  <input
+                    type="text"
+                    placeholder="Username..."
+                    className="username-focus"
+                  />
+                </div>
+                <div className="login-password">
+                  <input type="password" placeholder="New Password..." />
+                  <BiSolidHide className="login-password-hide-icon" />
+                </div>
+                <div className="login-password">
+                  <input type="password" placeholder="Confirm Password..." />
+                  <BiSolidHide className="login-password-hide-icon" />
+                </div>
+                <div className="login-button register-button">
+                  <h1>Update password</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,9 +9,11 @@ import {
   faTwitter,
   faSearchengin,
 } from "@fortawesome/free-brands-svg-icons";
+import Login from "./Login";
 const Navbar = () => {
   // Step 1: Create a state variable to manage menu visibility
   const [menuOpen, setMenuOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
   // Step 2: Define the handleClick function to toggle menu visibility
   const handleClick = () => {
@@ -21,52 +23,71 @@ const Navbar = () => {
     setMenuOpen(!menuOpen);
   };
   return (
-    <div className="container">
-      <div className="search-container">
-        <FontAwesomeIcon icon={faSearchengin} className="icon" />
-        <input type="text" placeholder="Search..." />
-      </div>
-      <h2 className="nameOfBanner">OMY-BLOGS</h2>
-      <div className="icons">
-        <FontAwesomeIcon className="iconfa" icon={faTwitter} />
-        <FontAwesomeIcon className="iconfa" icon={faFacebook} />
-        <FontAwesomeIcon className="iconfa" icon={faInstagram} />
-      </div>
-      <div className="menu-click">
-        {/* <FontAwesomeIcon icon={faBars} className="menu-icon" />
+    <>
+      <div className="container">
+        <div className="search-container">
+          <FontAwesomeIcon icon={faSearchengin} className="icon" />
+          <input type="text" placeholder="Search..." />
+        </div>
+        <h2 className="nameOfBanner">OMY-BLOGS</h2>
+        <div className="icons">
+          <FontAwesomeIcon className="iconfa" icon={faTwitter} />
+          <FontAwesomeIcon className="iconfa" icon={faFacebook} />
+          <FontAwesomeIcon className="iconfa" icon={faInstagram} />
+        </div>
+        <div className="menu-click">
+          {/* <FontAwesomeIcon icon={faBars} className="menu-icon" />
         <AiOutlineClose className="close-icon" /> */}
-        <img
-          src={menuOpen ? XImage : MenuImage}
-          alt={menuOpen ? "Close" : "Menu"}
-          className={menuOpen ? "close" : "menu"}
-          onClick={handleClick} // Step 3: Attach the handleClick function to the button
-        />
-      </div>
-      <div className={`side-nav ${menuOpen ? "open" : "close"}`}>
-        <div className="show-media">
-          <h2 className="nameOfBanner-side">OMY-BLOGS</h2>
-          <div className="icons-side">
-            <FontAwesomeIcon className="iconfa" icon={faTwitter} />
-            <FontAwesomeIcon className="iconfa" icon={faFacebook} />
-            <FontAwesomeIcon className="iconfa" icon={faInstagram} />
+          <img
+            src={menuOpen ? XImage : MenuImage}
+            alt={menuOpen ? "Close" : "Menu"}
+            className={menuOpen ? "close" : "menu"}
+            onClick={handleClick} // Step 3: Attach the handleClick function to the button
+          />
+        </div>
+        <div className={`side-nav ${menuOpen ? "open" : "close"}`}>
+          <div className="show-media">
+            <h2 className="nameOfBanner-side">OMY-BLOGS</h2>
+            <div className="icons-side">
+              <FontAwesomeIcon className="iconfa" icon={faTwitter} />
+              <FontAwesomeIcon className="iconfa" icon={faFacebook} />
+              <FontAwesomeIcon className="iconfa" icon={faInstagram} />
+            </div>
+          </div>
+          <div className="menus">
+            <Link to="/" onClick={closeMenu}>
+              <h2 className="home">Home</h2>
+            </Link>
+            <Link to="/aboutus" onClick={closeMenu}>
+              <h2>About us</h2>
+            </Link>
+            <Link to="/contactus" onClick={closeMenu}>
+              <h2>Contact us</h2>
+            </Link>
+            {/* <Link to="/Login" onClick={closeMenu}> */}
+            {/* <Link
+              to=""
+              onClick={() => {
+                setModalOpen(true);
+                closeMenu();
+              }}
+            >
+              <h2 className="login">LOGIN</h2>
+            </Link> */}
+            <button
+              className="login-btn login-nav"
+              onClick={() => {
+                setModalOpen(true);
+                closeMenu();
+              }}
+            >
+              Login
+            </button>
           </div>
         </div>
-        <div className="menus">
-          <Link to="/" onClick={closeMenu}>
-            <h2 className="home">Home</h2>
-          </Link>
-          <Link to="/aboutus" onClick={closeMenu}>
-            <h2>About us</h2>
-          </Link>
-          <Link to="/contactus" onClick={closeMenu}>
-            <h2>Contact us</h2>
-          </Link>
-          <Link to="/Login" onClick={closeMenu}>
-            <h2 className="login">LOGIN</h2>
-          </Link>
-        </div>
       </div>
-    </div>
+      {modalOpen && <Login setOpenModal={setModalOpen} />}
+    </>
   );
 };
 
