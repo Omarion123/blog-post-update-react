2fce46a7f64a7c55f968880cacfac0ff471c5d87
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 <anzymen@gmail.com>
2023-10-24 09:03:25 +0200

    adding and update

--- src/components/Update.js
+++ src/components/Update.js
@@ -1,6 +1,16 @@
-import React from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFetch";
+import React, { useEffect, useState } from "react";
+import { ImAttachment } from "react-icons/im";
+import { MdSubtitles } from "react-icons/md";
+import { BiSolidCategoryAlt } from "react-icons/bi";
+import { FaHeading } from "react-icons/fa";
+import { CgDetailsMore } from "react-icons/cg";
+// import { BsPersonBadgeFill } from "react-icons/bs";
+import { IoIosAdd } from "react-icons/io";
+import toast from "react-hot-toast";
+
+import { useHistory } from "react-router-dom";
 const Update = () => {
   const { _id } = useParams();
   console.log(_id);
@@ -9,6 +19,88 @@ const Update = () => {
     isPending,
     error,
   } = useFetch(`https://lastlast.onrender.com/api/post/one/${_id}`);
+  const history = useHistory();
+
+  useEffect(() => {
+    let email = sessionStorage.getItem("email");
+    let role = sessionStorage.getItem("role");
+    if (email === "" || (email === null && role !== "admin")) history.push("/");
+  }, []);
+  const [image, setImage] = useState(null);
+  console.log(image);
+  const [category, setCategory] = useState("");
+  // const [author, setAuthor] = useState("");
+  const createdAt = new Date().toISOString().slice(0, 10);
+
+  const [title, setTitle] = useState("");
+  const [heading, setHeading] = useState("");
+  const [description, setDescription] = useState("");
+  const [ispending, setIspending] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+
+    // Append your form fields to the FormData object
+    formData.append("image", image); // Append the image
+    formData.append("category", category);
+    formData.append("createdAt", createdAt);
+    formData.append("title", title);
+    formData.append("header", heading);
+    formData.append("description", description);
+
+    setIspending(true);
+
+    // Retrieve the token from localStorage
+    const token = localStorage.getItem("token");
+
+    console.log("token", token);
+
+    // Check if the token is available
+    if (token) {
+      // No need to set Content-Type explicitly; FormData sets it automatically
+      // Create headers with the token
+      const headers = {
+        Authorization: `Bearer ${token}`,
+      };
+
+      // Make the fetch request with the headers and FormData
+      fetch(`https://lastlast.onrender.com/api/post/one/${_id}`, {
+        method: "UPDATE",
+        headers: headers,
+        body: formData, // Use FormData to send the data
+      })
+        .then((response) => {
+          if (response.ok) {
+            setImage(null);
+            setTitle("");
+            setImage(null);
+            setCategory("");
+            setHeading("");
+            setDescription("");
+            setIspending(false);
+            console.log("blog added");
+            toast.success("Blog updated successfully");
+            // Request was successful
+            return response.json();
+          } else {
+            // Handle error
+            setIspending(false);
+            console.error("Request failed with status:", response.status);
+            toast.error("Request failed with status:", response.status);
+            // You can also handle specific error codes here
+          }
+        })
+        .catch((error) => {
+          // Handle fetch errors
+          setIspending(false);
+          toast.error("Fetch error:", error);
+        });
+    } else {
+      console.error("Token not found in localStorage. Please log in.");
+      toast.error("Token not found in localStorage. Please log in.");
+    }
+  };
   return (
     <div className="update">
       <h1>Updated</h1>
@@ -16,24 +108,86 @@ const Update = () => {
       {error && <div>{error}</div>}
       {blog && (
         <>
-          <form>
-            <label>image</label>
-            <input type="image" value={blog.title} />
-            <br></br>
-            <label>title</label>
-            <input type="image" value={blog.title} />
-            <br></br>
-            <label>category</label>
-            <input type="image" value={blog.category} />
-            <br></br>
-            <label>header</label>
-            <input type="image" value={blog.header} />
-            <br></br>
-            <label>description</label>
-            <input type="image" value={blog.description} />
-            <br></br>
-            <br></br>
-            <button type="submit">submit</button>
+          <form onSubmit={handleSubmit}>
+            <h2>Add blog here!</h2>
+            <div className="addblog-container">
+              <div className="image-select">
+                <ImAttachment className="icon" />
+                <input
+                  type="file"
+                  required
+                  placeholder="Insert image..."
+                  // value={image}
+                  // onChange={(e) => setImage(e.target.value)}
+                  onChange={(e) => setImage(e.target.files[0])}
+                  className="file"
+                />
+              </div>
+              <div className="image-select">
+                <BiSolidCategoryAlt className="icon" />
+                <input
+                  type="text"
+                  required
+                  value={blog.category}
+                  onChange={(e) => setCategory(e.target.value)}
+                />
+              </div>
+              {/* <div className="image-select">
+            <BsPersonBadgeFill className="icon" />
+            <input
+              type="text"
+              required
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+            />
+          </div> */}
+              <div className="image-select">
+                <MdSubtitles className="icon" />
+                <input
+                  type="text"
+                  required
+                  value={blog.title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+              <div className="image-select">
+                <FaHeading className="icon" />
+                <input
+                  type="text"
+                  required
+                  value={blog.header}
+                  onChange={(e) => setHeading(e.target.value)}
+                />
+              </div>
+              <div className="image-select">
+                <CgDetailsMore className="icon" />
+                {/* <input type="text-area" placeholder="Enter content..." /> */}
+                <textarea
+                  rows="4"
+                  required
+                  value={blog.description}
+                  onChange={(e) => setDescription(e.target.value)}
+                ></textarea>
+              </div>
+              <div className="addbtn">
+                {!ispending && (
+                  <button>
+                    <div className="add-blog-button">
+                      <h1>Update blog</h1>
+                      <IoIosAdd className="icon-add" />
+                    </div>
+                  </button>
+                )}
+                {ispending && (
+                  <button>
+                    <div className="add-blog-button">
+                      <h1>Updating blog...</h1>
+                      <IoIosAdd className="icon-add" />
+                    </div>
+                  </button>
+                )}
+              </div>
+            </div>
           </form>
         </>
       )}
