8ee5cf585ede1a2e399baac6e221e586d1712ef7
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 <anzymen@gmail.com>
2023-10-30 23:48:33 +0200

    added tweak data

--- src/components/charts/Linechart.js
+++ src/components/charts/Linechart.js
@@ -12,46 +12,34 @@ import {
 
 const data = [
   {
-    name: "Page A",
+    name: "Posts",
     uv: 4000,
     pv: 2400,
     amt: 2400,
   },
   {
-    name: "Page B",
+    name: "Views",
     uv: 3000,
     pv: 1398,
     amt: 2210,
   },
   {
-    name: "Page C",
+    name: "Comments",
     uv: 2000,
     pv: 9800,
     amt: 2290,
   },
   {
-    name: "Page D",
+    name: "Categories",
     uv: 2780,
     pv: 3908,
     amt: 2000,
   },
   {
-    name: "Page E",
+    name: "Users",
     uv: 1890,
     pv: 4800,
     amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
   },
 ];
 
